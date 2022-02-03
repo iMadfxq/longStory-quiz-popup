29bb576b0ce5c181cc0684639c4ddfef1fa845ab
iMadfxq <imadfxq@gmail.com>
2022-02-03 15:38:33 -0600

    Finished webpack configuration, built and working correctly

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const { mainModule } = require('process');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+  resolve: {
+    extensions: ['.js']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [MiniCssExtractPlugin.loader, 
+        'css-loader',
+        'sass-loader'
+      ],
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      inject: true,
+      template: './src/html/index.html',
+      filename: 'index.html'
+    }),
+    new MiniCssExtractPlugin({
+        filename: 'style.css',
+      },
+    ),
+    new CleanWebpackPlugin(),
+  ]
+};
